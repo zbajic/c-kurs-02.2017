63d0b8e6d46407a49e433664a4e49a950f7ca144
ladyguda <rtrk@rtrkbl103-lin>
2017-05-24 15:43:15 +0200

    verzija sa rezultatom trke i ispisom u fajl

--- horseRace.c
+++ horseRace.c
@@ -2,18 +2,22 @@
 #include <pthread.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 
 
 #define NUMBER_OF_LANES 10        // number of rows in matrix
-#define RACE_LENGTH 50   // number of columns in matrix
+#define RACE_LENGTH 500   // number of columns in matrix
 
 
 typedef struct horse
 {
     float fitness;
     int horse_id;       //horse id index
-    short q_factor;     //quit race if q_factor >10
+    int h_result;
+    int h_step;
+    short q_factor;    //quit race if q_factor >10
     char name[10];
 }horse;
 
@@ -23,11 +27,12 @@ typedef struct horse_step
 	short step;
 }horse_step;
 
+pthread_t workers[NUMBER_OF_LANES];
 horse race_track[NUMBER_OF_LANES][RACE_LENGTH]; //matrix
 horse h_array[NUMBER_OF_LANES];
 horse_step hs_array[NUMBER_OF_LANES];
 int temp[NUMBER_OF_LANES];
-int test[RACE_LENGTH];
+int test[10][RACE_LENGTH];
 
 
 
@@ -85,6 +90,18 @@ void* process_positions(void* param)
 
 }
 
+void* process_result(void* param)
+{
+	horse* hs = (horse*)param;
+	FILE* fp=fopen("nesto.txt","a+");
+
+	fprintf(fp, "%d(%d)",hs->h_result,hs->h_step);
+	fclose(fp);
+
+
+
+}
+
 
 
 
@@ -105,26 +122,42 @@ void* run(void* param)
     int column;
 
 
-
+    int j=0;
     int step = 0;
     int k=0;
+
 
     for(column = 0; column < RACE_LENGTH; column += step)
     {
 
-//    	printf("%d ",data -> horse_id);
 
         race_track[data -> horse_id][column] = *data; // upisujemo konja u matricu
+        data -> h_result = column;
 
-        test[k]=data->horse_id;
+       test[data -> horse_id][column]=data->horse_id;
+
         k++;
 
         step = make_step(data -> fitness);
+    	printf("%d(%d)[%d] ",data -> horse_id,column,step);
+
+       if (column + step >= RACE_LENGTH)
+       {
+
+    	    printf("%d ",data -> horse_id);
+    	    data->h_result = data ->horse_id;
+    	    data->h_step = step;
+        	pthread_t res_thread;
+        	pthread_create(&res_thread, NULL, process_result, (void *)data);
+
+
+        }
 
         sleep(1);
 
     }
-    pthread_exit(0);
+
+   // pthread_exit(0);
 
 
 }
@@ -135,32 +168,30 @@ int main()
     int i,j,k;
 
     horse horse1,horse2,horse3,horse4,horse5,horse6,horse7,horse8,horse9,horse10;
-    horse1.fitness=10;
+    horse1.fitness=50;
     horse2.fitness=20;
     horse3.fitness=30;
     horse4.fitness=40;
-    horse5.fitness=50;
-    horse6.fitness=60;
-    horse7.fitness=70;
+    horse5.fitness=60;
+    horse6.fitness=23;
+    horse7.fitness=40;
     horse8.fitness=80;
-    horse9.fitness=90;
-    horse10.fitness=100;
+    horse9.fitness=10;
+    horse10.fitness=20;
 
     //use this array to test
     horse h_array[] = {horse1,horse2,horse3,horse4,horse5,horse6,horse7,horse8,horse9,horse10};
 
 
-    int thread_counter = 0;
-
     //threads
-    pthread_t workers[NUMBER_OF_LANES];
+   // pthread_t workers[NUMBER_OF_LANES];
 
     for (j = 0; j < NUMBER_OF_LANES; j++){
 
         h_array[j].horse_id = j;
 
-        pthread_create(&workers[thread_counter], NULL, run, (void*)(h_array + j));
-        thread_counter++;
+        pthread_create(&workers[j], NULL, run, (void*)(h_array + j));
+
     }
 
     //join all threads
@@ -168,21 +199,17 @@ int main()
         pthread_join(workers[i], NULL);
     }
 
-    for (j = 0; j < RACE_LENGTH; j++){
-
-    	printf("%d ",test[j]);
-    }
-
-  /*  //print matrix
+    printf("\n");
+    //print matrix
     for (i = 0; i < NUMBER_OF_LANES; i++)
     {
         for (j = 0; j < RACE_LENGTH; j++)
         {
-            printf("%g ",race_track[i][j].fitness);
+            printf("%d ",test[i][j]);
         }
         printf("\n");
     }
-*/
+
 
 
     return 0;
