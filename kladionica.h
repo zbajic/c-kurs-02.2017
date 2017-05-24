5e022d1e7aec8f0af22e5235ce468d964cc444b0
sloboilic <slobodanilic@rocketmail.com>
2017-05-24 16:51:00 +0200

    RACE metod za konji auta

---
+++ kladionica.h
@@ -0,0 +1,36 @@
+/*
+ * kladionica.h
+ *
+ *  Created on: May 24, 2017
+ *      Author: rtrk
+ */
+
+
+/*
+ * kladionica.h
+
+ *
+ *  Created on: May 23, 2017
+ *      Author: rtrk
+ */
+#include <stdio.h>
+#ifndef _KLADIONICA_H
+#define _KLADIONICA_H
+
+typedef struct str {
+	char tip;
+	int brTr;
+	int redBr;
+	char* ime;
+}STR;
+
+void Rase();
+void RaseA();
+void ispisK();
+void introK();
+void ispisA();
+void introA();
+
+
+#endif
+
