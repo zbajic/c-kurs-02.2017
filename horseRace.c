f737f9bcf8b3a69103feb08380d3daa974a18823
ladyguda <rtrk@rtrkbl103-lin>
2017-05-25 12:51:19 +0200

    verzija sa procesiranjem jednog fajla i upis rezultata trke u bafer bez niti

--- horseRace.c
+++ horseRace.c
@@ -3,7 +3,8 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "header.h"
+#include "N1.h"
 
 
 
@@ -11,28 +12,35 @@
 #define RACE_LENGTH 500   // number of columns in matrix
 
 
-typedef struct horse
-{
-    float fitness;
-    int horse_id;       //horse id index
-    int h_result;
-    int h_step;
-    short q_factor;    //quit race if q_factor >10
-    char name[10];
-}horse;
-
 typedef struct horse_step
 {
 	short horse_id;
 	short step;
 }horse_step;
 
+/*
+typedef struct race
+{
+	char c = 'K';
+	char* datum[1];
+	char* ime[NUMBER_OF_LANES];
+}race;
+*/
+
+race race_result;
+buffer_struct buf_result;
+
+
+
+//char* race_date[1];
 pthread_t workers[NUMBER_OF_LANES];
 horse race_track[NUMBER_OF_LANES][RACE_LENGTH]; //matrix
 horse h_array[NUMBER_OF_LANES];
 horse_step hs_array[NUMBER_OF_LANES];
 int temp[NUMBER_OF_LANES];
 int test[10][RACE_LENGTH];
+short name_index;
+char* race_date;
 
 
 
@@ -53,20 +61,40 @@ int make_step(float fitness) //parameter fitness for making step
 
 
 
-int start_race()  //main condition to start_race if <30
+int start_race_check(horse h_arr)  //main condition to start_race if <30
 {
     int i;
     int sum = 0;
     for (i = 0; i < NUMBER_OF_LANES; i++)
     {
-        sum += h_array[i].q_factor;
+        sum += h_arr.q_factor;
     }
-    if (sum > 30)
-        return 0;
+    if (sum >= 30)
+        return 0; //race is canceled
 
-    return 1;
+    return 1; //race will start
 }
 
+void print_canceled_race(horse h_arr[])
+{
+	char* f_name = strcat(strcat("race_", race_date), ".txt");
+
+	FILE* fp=fopen(f_name,"w+");
+	fprintf(fp, "Date: %s\n",race_date);
+	int i=0;
+	for(i=0;i<NUMBER_OF_LANES; i++)
+	{
+		fprintf(fp, "%d - %s\n",i, h_arr[i].name);
+	}
+	fclose(fp);
+	printf("Date: %s/n", race_date);
+	int j=0;
+	for( j=0;j<NUMBER_OF_LANES; j++)
+		{
+			printf("%d - %s\n",j, h_arr[j].name);
+		}
+
+}
 
 
 
@@ -86,20 +114,37 @@ void* process_positions(void* param)
 
 //	hs_array[hs -> horse_id] = hs -> step;
 
-
+return NULL;
 
 }
 
+
 void* process_result(void* param)
 {
-	horse* hs = (horse*)param;
-	FILE* fp=fopen("nesto.txt","a+");
+	if(name_index < NUMBER_OF_LANES	)
+	{
+		horse* hs = (horse*)param;
+		/*
+		 *FILE* fp=fopen("nesto.txt","a+");
+		fprintf(fp, "%d(%d)",hs->h_result,hs->h_step);
+		fclose(fp);
+		 */
+		strcpy(race_result.date, race_date);
 
-	fprintf(fp, "%d(%d)",hs->h_result,hs->h_step);
-	fclose(fp);
+		race_result.name[name_index++] = hs -> name;
+	}
+	else
+	{
+		//filling circular buffer with race results
+		// or writing into files if circular buffer is full
+
+		put(&buf_result, race_result);
+		dump(&buf_result);
 
 
+	}
 
+	return NULL;
 }
 
 
@@ -129,37 +174,27 @@ void* run(void* param)
 
     for(column = 0; column < RACE_LENGTH; column += step)
     {
-
-
-        race_track[data -> horse_id][column] = *data; // upisujemo konja u matricu
+        race_track[data -> horse_id][column] = *data; // writing horse into matrix
         data -> h_result = column;
-
-       test[data -> horse_id][column]=data->horse_id;
-
+        test[data -> horse_id][column]=data->horse_id;
         k++;
-
         step = make_step(data -> fitness);
-    	printf("%d(%d)[%d] ",data -> horse_id,column,step);
-
+    //	printf("%d(%d)[%d] ",data -> horse_id,column,step);
        if (column + step >= RACE_LENGTH)
        {
-
     	    printf("%d ",data -> horse_id);
     	    data->h_result = data ->horse_id;
     	    data->h_step = step;
         	pthread_t res_thread;
         	pthread_create(&res_thread, NULL, process_result, (void *)data);
-
-
         }
 
         sleep(1);
-
     }
 
    // pthread_exit(0);
 
-
+    return NULL;
 }
 
 
@@ -167,7 +202,7 @@ int main()
 {
     int i,j,k;
 
-    horse horse1,horse2,horse3,horse4,horse5,horse6,horse7,horse8,horse9,horse10;
+ /*   horse horse1,horse2,horse3,horse4,horse5,horse6,horse7,horse8,horse9,horse10;
     horse1.fitness=50;
     horse2.fitness=20;
     horse3.fitness=30;
@@ -180,12 +215,26 @@ int main()
     horse10.fitness=20;
 
     //use this array to test
-    horse h_array[] = {horse1,horse2,horse3,horse4,horse5,horse6,horse7,horse8,horse9,horse10};
+    horse h_array[] = {horse1,horse2,horse3,horse4,horse5,horse6,horse7,horse8,horse9,horse10};*/
+
+    // TO DO  - getting horses for race from race-files  FUNCTION
 
 
+
+    Init(&buf_result); //preparing circular buffer
+    file_process();
+
+
+
+
+/*if(!(start_race_check(&h_arr))
+	{
+		print_canceled_race(&h_arr);
+	}
+//else;
+*/
     //threads
    // pthread_t workers[NUMBER_OF_LANES];
-
     for (j = 0; j < NUMBER_OF_LANES; j++){
 
         h_array[j].horse_id = j;
@@ -200,7 +249,7 @@ int main()
     }
 
     printf("\n");
-    //print matrix
+  /*  //print matrix
     for (i = 0; i < NUMBER_OF_LANES; i++)
     {
         for (j = 0; j < RACE_LENGTH; j++)
@@ -209,8 +258,6 @@ int main()
         }
         printf("\n");
     }
-
-
-
+*/
     return 0;
 }
---
+++ proba11.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
+
+horse h_array[10];
+char* race_date;
+
+void file_process() {
+	char line[MAXSZ], dat[MAXSZ], name[MAXSZ];
+	short fitness;
+	int qfact;
+	int r_number;
+	FILE* race1;
+	horse* node;
+	int i = 0;
+	horse* first = NULL;
+	horse* last = NULL;
+	char* date;
+	race1 = fopen("race1.txt", "r"); //otvaranje fajla
+	if (race1 == NULL) {
+		printf("Cannot open file\n");
+// return 1;
+	}
+	while (fgets(line, sizeof(line), race1) != NULL) { // Parsiranje svakog reda
+		sscanf(line, "%s %s %d %d", dat, name, &fitness, &qfact);
+		node = malloc(sizeof(horse));
+//Alociranje novog noda za cuvanje podataka
+		if (node == NULL) {
+			printf("Ran out of memory\n");
+// return 1;
+		}
+		race_date = strdup(dat);
+		strcpy(node->name, strdup(name));
+	//	node->name = strdup(name);
+		node->fitness = fitness;
+		if (qfact == 10) {
+			node->q_factor = 1 + random() % 8;
+		} else {
+			node->q_factor = qfact;
+		}
+		node->next = NULL;
+		if (first != NULL) {
+			last->next = node;
+			last = node;
+		} else {
+			first = node;
+			last = node;
+		}
+	}
+	fclose(race1);
+	node = first; // Testiranje izlaza
+	while (node != NULL) {
+		/* printf ("%s %s %d %d \n",
+		 date, node->name, node->fitness, node->qfact);*/
+		h_array[i] = *node;
+		//race_date = date;
+		i++;
+		node = node->next;
+	}
+
+}
