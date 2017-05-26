e09f9f24991eefe4a2f3ae87202ef96da4f311a4
ladyguda <rtrk@rtrkbl103-lin>
2017-05-26 15:43:37 +0200

    razliciti koraci, problem sa upisom u fajl

--- horceRaceLive.c
+++ horceRaceLive.c
@@ -1,4 +1,3 @@
-/*
 #include <stdio.h>
 #include <pthread.h>
 #include <math.h>
@@ -8,14 +7,14 @@
 #include "horceRaceCBuffer.h"
 #include "horseRacefileProcess.h"
 
-
+/*
 typedef struct race
 {
 	char c = 'K';
 	char* datum[1];
 	char* ime[NUMBER_OF_LANES];
 }race;
-
+*/
 
 race race_result;// structure for putting results of race in circular buffer
 buffer_struct buf_result;
@@ -31,6 +30,19 @@ horse_step h_step_array[NUMBER_OF_LANES]; // saving horse steps - 10 steps for 1
 int testID[NUMBER_OF_LANES][RACE_LENGTH]; // horse ID's
 short name_index;// used in process_result() for index of names
 char* race_date; // date of current race
+
+int flags[10]; // provjera da li je skocio
+int koraci[10]; // pamtimo korake
+int ukupno[10];
+int var=0;
+int posalji[10];
+int horseID[10];
+
+
+pthread_mutex_t     mutex = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_cond_t      cond  = PTHREAD_COND_INITIALIZER;
+
 
 // STEP GENERATOR FOR HORSES
 int make_step(float fitness) //parameter fitness for making step
@@ -93,13 +105,30 @@ void print_canceled_race()
 }
 
 
+
 // SAVING HORSE STEPS FOR ALL HORSES
-void* process_positions(void* param)
+void process_positions()
 {
-	horse_step* hs = (horse_step*)param;
-    //hs_array[hs -> horse_id] = hs -> step;
-	return NULL;
+	int i;
+	char f_name[] = "race_";
+	char date[12];
+	for (i = 0; i < 12; i++)
+		{
+			date[i] = *(race_date + i);
+		}
+		strcat(f_name, date);
+
+		FILE* fp = fopen("aaaaaa.txt", "w+");
+
+		fprintf(fp, "%s ","*" );
+
+	fclose(fp);
 }
+ /*make_string(int niz[]){
+	char*
+
+
+}*/
 
 // SAVING FINAL RESULT OF RACE IN BUFFER
 void* process_result(void* param)
@@ -107,11 +136,11 @@ void* process_result(void* param)
 	if(name_index < NUMBER_OF_LANES	)
 	{
 		horse* hs = (horse*)param;
-
+		/*
 		 *FILE* fp=fopen("nesto.txt","a+");
 		fprintf(fp, "%d(%d)",hs->h_result,hs->h_step);
 		fclose(fp);
-
+		 */
 		strcpy(race_result.date, race_date);
 		race_result.name[name_index++] = hs -> name;
 	}
@@ -126,11 +155,34 @@ void* process_result(void* param)
 }
 
 
+int sviSkocili(){
+	int i=0;
+	for(i=0;i<10;i++){
+		if(flags[i]!=1){
+			return 0;//bar jedan nije skocio
+		}
+	}
+	return 1;
+}
 
+
+void reset(){
+	int i=0;
+		for(i=0;i<10;i++){
+			flags[i]=0;
+
+		}
+
+
+}
+
+int end=10;
+int izadji;
+int t[10];
 // RUNNING HORSES - THREADS
 void* run(void* param)
 {
-
+    /*
 
     ovo je sad uspjelo da svaku nit tj svakog konja kojeg identifikujem
     preko horse_id pomjeri za neki korak koji se dobije primjenom funkcije
@@ -138,33 +190,81 @@ void* run(void* param)
 
     konj se nakon pomjeraja smjesta na odgovarajucu poziciju u matrici
 
+    */
+    while(end>0){
 
-
+	pthread_mutex_lock(&mutex);
     horse* data = (horse*)param;
-    int column;
-    int j = 0;
-    int step = 0;
+	//printf("%d\n", data->horse_id);
 
-    for(column = 0; column < RACE_LENGTH; column += step)
-    {
-        race_track[data -> horse_id][column] = *data; // writing horse into matrix - each step
+    if(sviSkocili()==1){
+    //	printf("svi =%d\n ", data->horse_id);
+    	reset();
+    	pthread_cond_broadcast(&cond);
+
+    }else if(flags[data->horse_id]==1){
+    	// printf("wait {%d}",data->horse_id);
+    	 pthread_cond_wait(&cond,&mutex);
+
+    }else //(flags[data->horse_id]==0)
+   {
+   // for(column = 0; column < RACE_LENGTH; column += step)
+   // {
+  // while(1){
+    	if(ukupno[data->horse_id]>=30){
+
+    		//printf("Stigao: %d ",data->horse_id);
+    		printf("h_id_exit = %d  step=%d \n",data->horse_id, ukupno[data->horse_id]);
+    		end--;
+    		izadji=200;
+    		flags[data -> horse_id]=1;
+    		 //pthread_setcancelstate(t_horses[data->horse_id]);
+    		// pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    	//pthread_cancel(t_horses[data->horse_id]);
+    	sleep(1);
+    	printf("otpisani=%d\n", data->horse_id);
+    	printf("end=%d\n",end);
+    	t[data->horse_id]=1;
+
+    	}else{
+
+    		race_track[data -> horse_id][ukupno[data->horse_id]] = *data;
+
+    		printf("h_id = %d  step=%d \n",data->horse_id, ukupno[data->horse_id]);
+
+
+    		horseID[data->horse_id] = ukupno[data->horse_id];
+
+    		ukupno[data -> horse_id] +=  make_step(data->fitness);
+
+            flags[data -> horse_id] = 1;
+
+  }
+    	}
+
+   /*     race_track[data -> horse_id][column] = *data; // writing horse into matrix - each step
         data -> h_result = column;
         testID[data -> horse_id][column] = data->horse_id;
 
-        step = make_step(data -> fitness);
+        step = make_step(data -> fitness);*/
+
+
     //	printf("%d(%d)[%d] ",data -> horse_id,column,step);
         // HORSE THAT FINISHED RACING
-       if (column + step >= RACE_LENGTH) // creating thread for storing result of the race
+     /*  if (column + step >= RACE_LENGTH) // creating thread for storing result of the race
        {
     	    printf("%d ",data -> horse_id);
     	    data->h_result = data ->horse_id;
     	    data->h_step = step;
         	pthread_t res_thread;
         	pthread_create(&res_thread, NULL, process_result, (void *)data);
-        }
-        sleep(1);
-    }
-
+        }*/
+   // }
+  // }
+   pthread_mutex_unlock(&mutex);
+    //   sleep(1);
+   }
+    printf("\n");
     return NULL;
 }
 
@@ -174,6 +274,22 @@ int main()
     int i;
 	int j;
 
+	 horse horse1,horse2,horse3,horse4,horse5,horse6,horse7,horse8,horse9,horse10;
+	    horse1.fitness=1;
+	    horse2.fitness=1;
+	    horse3.fitness=2;
+	    horse4.fitness=3;
+	    horse5.fitness=3;
+	    horse6.fitness=2;
+	    horse7.fitness=5;
+	    horse8.fitness=4;
+	    horse9.fitness=2;
+	    horse10.fitness=1;
+
+	    horse h_array[]={horse1,horse2,horse3,horse4,horse5,horse6,horse7,horse8,horse9,horse10};
+	//    process_positions();
+
+/*
     Init(&buf_result); //preparing circular buffer
     file_process(); // getting input from files - horses for race and race date
 
@@ -182,7 +298,7 @@ int main()
     	print_canceled_race();
     }
     else
-    {
+    {*/
     	for (j = 0; j < NUMBER_OF_LANES; j++)
     	{
     		h_array[j].horse_id = j;
@@ -198,19 +314,20 @@ int main()
         printf("\n");
 
 
-    //print matrix -for testing purpose
+
+  //  print matrix -for testing purpose
     for (i = 0; i < NUMBER_OF_LANES; i++)
     {
         for (j = 0; j < RACE_LENGTH; j++)
         {
-            printf("%d ",test[i][j]);
+            printf("%d ",race_track[i][j].horse_id);
         }
         printf("\n");
-    }
+   }
 
-    }
+
+
 
     return 0;
 
 }
-*/
---
+++ horseRacefileProcess.h
@@ -0,0 +1,32 @@
+/*
+ * header.h
+ *
+ * Created on: May 25, 2017
+ * Author: rtrk
+ */
+#ifndef HORSERACEFILEPROCESS_H_
+#define HORSERACEFILEPROCESS_H_
+
+#define MAXSZ 100
+#define NUMBER_OF_LANES 10  //  number of rows in matrix
+#define RACE_LENGTH 30   // number of columns in matrix
+
+typedef struct horse { //struktura vezana za Horse
+	float fitness;
+	int horse_id;       //horse  id index
+	int h_result;
+	int h_step;
+	short q_factor;    //quit race if q_factor >10
+	char name[10];
+	struct horse* next;
+} horse;
+
+typedef struct horse_step
+{
+	short horse_id;
+	short step;
+}horse_step;
+
+
+
+#endif /* HORSERACEFILEPROCESS_H_ */
