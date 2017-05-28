08cf706235d60236c4784ee28fa0e91a165c66a4
rtrk <rtrk@rtrkbl11-lin>
2017-05-28 20:03:18 +0200

    Dodavanje fajla kladionica.h

---
+++ kladionica2/kladionica.h
@@ -0,0 +1,22 @@
+#ifndef _KLADIONICA_H
+#define _KLADIONICA_H
+
+#include <stdio.h>
+#include "CircularBuffer.h"
+#include "TipTrka.h"
+
+void Race();
+void RaceA();
+void RaceK();
+void ispisKonja();
+void parsiranjeK();
+void parsiranjeA();
+void introK();
+void ispisA();
+void introA();
+void sort(int*,int*,int);
+void printRace(trka*);
+void* Punjenje_bafera(void* pParam);
+void* Results(void* pParam);
+
+#endif
