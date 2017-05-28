6fee74b626a706a8645bcd6ed69918d63c2fcd42
rtrk <rtrk@rtrkbl11-lin>
2017-05-28 13:35:11 +0200

    Struktura koja sadrzi podatke o trci

---
+++ kladionica2/TipTrka.h
@@ -0,0 +1,16 @@
+#ifndef TIPTRKA_H_
+#define TIPTRKA_H_
+
+#include <stdio.h>
+
+/* Struktura u koju se upisuju podaci o jednoj trci */
+
+typedef struct _trka
+{
+	 char c;				/* prvi karakter oznacava da li se radi o hipodromu ili autotrci*/
+	 char datum[12];		/* datum trke */
+	 char* ime[10];			/* pozicija  ucesnika */
+ }trka;
+
+
+#endif //TIPTRKA_H_
